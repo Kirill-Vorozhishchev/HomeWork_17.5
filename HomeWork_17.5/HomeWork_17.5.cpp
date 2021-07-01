a3d0df9874d234c03929ea4c6ca71a4e184bf9cb
Kirill Vorozhishchev <1507kirill1998@mail.ru>
2021-07-01 20:54:22 +0700

    Добавьте файлы проекта.

---
+++ HomeWork_17.5/HomeWork_17.5.cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+
+class vector
+{
+public:
+	vector() : x(10), y(10), z(10)
+	{}
+	vector(double _x, double _y, double _z) : x(_x), y(_y), z(_z)
+	{}
+	void show()
+	{
+		std::cout << x << ' ' << y << ' ' << z << '\n';
+	}
+private:
+	double x;
+	double y;
+	double z;
+};
+
+class example
+{
+private:
+	int a;
+
+public:
+	example()
+	{
+		a = 0;
+	}
+
+	example(int newA)
+	{
+		a = newA;
+	}
+
+	int GetA()
+	{
+		return 5;
+	}
+
+	void SetA(int newA)
+	{
+		a = newA;
+	}
+};
+
+int main()
+{
+	vector v;
+	v.show();
+
+	example temp(5);
+	std::cout << temp.GetA();
+}
